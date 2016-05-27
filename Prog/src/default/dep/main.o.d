64de93649f81dc173065da493b94989e703e1c48
74ls00 <userled@yandex.ru>
2016-05-27 23:17:28 +0300

    r001

---
+++ Prog/src/default/dep/main.o.d
@@ -0,0 +1,76 @@
+main.o: ../main.c c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom168.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iomx8.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/wdt.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/string.h ../RGBLED.h \
+  ../softPWM.h ../speed.h ../WorkMode.h ../KeyScan.h ../setup.h \
+  G:\home\Documents\Projects\0-Electronics\OsuKB\osu-keyboard-v2\Prog\src\usbdrv/usbdrv.h \
+  G:\home\Documents\Projects\0-Electronics\OsuKB\osu-keyboard-v2\Prog\src\usbdrv/../usbconfig.h \
+  G:\home\Documents\Projects\0-Electronics\OsuKB\osu-keyboard-v2\Prog\src\usbdrv/usbportability.h
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom168.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iomx8.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/wdt.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/string.h:
+
+../RGBLED.h:
+
+../softPWM.h:
+
+../speed.h:
+
+../WorkMode.h:
+
+../KeyScan.h:
+
+../setup.h:
+
+G:\home\Documents\Projects\0-Electronics\OsuKB\osu-keyboard-v2\Prog\src\usbdrv/usbdrv.h:
+
+G:\home\Documents\Projects\0-Electronics\OsuKB\osu-keyboard-v2\Prog\src\usbdrv/../usbconfig.h:
+
+G:\home\Documents\Projects\0-Electronics\OsuKB\osu-keyboard-v2\Prog\src\usbdrv/usbportability.h:
---
+++ Prog/src/default/dep/usbdrv.o.d
@@ -0,0 +1,52 @@
+usbdrv.o: ../usbdrv/usbdrv.c \
+  G:\home\Documents\Projects\0-Electronics\OsuKB\osu-keyboard-v2\Prog\src\./usbdrv/usbdrv.h \
+  G:\home\Documents\Projects\0-Electronics\OsuKB\osu-keyboard-v2\Prog\src\./usbdrv/../usbconfig.h \
+  G:\home\Documents\Projects\0-Electronics\OsuKB\osu-keyboard-v2\Prog\src\./usbdrv/usbportability.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom168.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iomx8.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  G:\home\Documents\Projects\0-Electronics\OsuKB\osu-keyboard-v2\Prog\src\./usbdrv/oddebug.h
+
+G:\home\Documents\Projects\0-Electronics\OsuKB\osu-keyboard-v2\Prog\src\./usbdrv/usbdrv.h:
+
+G:\home\Documents\Projects\0-Electronics\OsuKB\osu-keyboard-v2\Prog\src\./usbdrv/../usbconfig.h:
+
+G:\home\Documents\Projects\0-Electronics\OsuKB\osu-keyboard-v2\Prog\src\./usbdrv/usbportability.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom168.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iomx8.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+G:\home\Documents\Projects\0-Electronics\OsuKB\osu-keyboard-v2\Prog\src\./usbdrv/oddebug.h:
